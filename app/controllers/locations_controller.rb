d01dd041783838ae530afbf9de22d0278f14e390
NaveedG <nganjani@protonmail.com>
2020-01-25 10:55:13 -0600

    show and find_forecasts actions added to locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,24 @@
+require 'rest-client'
+
 class LocationsController < ApplicationController
 
   def index
     @locations = Location.all 
     render json: @locations
+  end
+
+  def show
+    @location = Location.find(params[:id])
+    render json: @location
+  end
+
+  def find_forecasts
+    @location = Location.find_by(key: params[:key])
+    forecasts = []
+    response = RestClient.get("http://dataservice.accuweather.com/forecasts/v1/daily/5day/#{@location.key}", { params: { apikey: Rails.application.credentials.dig(:api_key) }})
+    weather_data = JSON.parse(response)
+    forecasts.push(weather_data['DailyForecasts'])
+    render json: forecasts[0][1..3]
   end
 
 end
